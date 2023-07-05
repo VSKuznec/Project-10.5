6249bc8e9330377b556c8de683d56980d2bb166a
VS_KUZNEC <sevactet.games@gmail.com>
2023-07-05 16:07:55 +0300

    Добавьте файлы проекта.

---
+++ Project 10.5.1/Program.cs
@@ -0,0 +1,80 @@
+﻿using Project_10._5._1;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Task1
+{
+    class Program
+    {
+        static ILogger Logger { get; set; } 
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+            var worker1 = new Worker1(Logger);
+            var worker2 = new Worker2(Logger);
+            var worker3 = new Worker3(Logger);
+
+            worker1.Work();
+            worker2.Work();
+            worker3.Work();
+
+            Console.ReadKey();
+
+            try
+            {
+                ICalculator calculator = new Calculator();
+                Console.WriteLine("Введите первое число: ");
+                double a = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число: ");
+                double b = double.Parse(Console.ReadLine());
+
+                double result = calculator.Calculate(a, b);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка. Вы что-то сделали не так.");
+            }
+        }
+    } 
+
+    public interface ICalculator 
+    {
+        double Calculate(double numberOne, double numberTwo); 
+    }
+
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+
+    public class Calculator: ICalculator {        
+        public double Calculate(double numberOne, double numberTwo) 
+        {            
+            return numberOne + numberTwo;
+        }
+    }
+
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface IWorker
+    {
+        void Work();
+    }
+}
---
+++ Project 10.5.1/Worker1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace Project_10._5._1
+{
+    public class Worker1 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker1(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker 1 начал свою работу");
+            Thread.Sleep(3000);
+            Logger.Event("Worker 1 окончил свою работу");
+        }
+    }
+
+    public class Worker2 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker2(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker 2 начал свою работу");
+            Thread.Sleep(3000);
+            Logger.Event("Worker 2 окончил свою работу");
+        }
+    }
+
+    public class Worker3 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker3(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker 3 начал свою работу");
+            Thread.Sleep(3000);
+            Logger.Event("Worker 3 окончил свою работу");
+        }
+    }
+}
